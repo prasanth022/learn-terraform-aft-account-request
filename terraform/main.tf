2ba9c2c10a46b02f8b5dafd666992a9a017fd7fa
prasanth022 <prasanth.pichali@gmail.com>
2022-09-12 17:05:39 +0530

    checking run

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL1>"
+    AccountEmail              = "<ACCOUNT EMAIL>"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "<SSO EMAIL>"
